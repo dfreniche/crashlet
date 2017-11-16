91583af4f82668cf83d0cb6f65c30b3953e3085d
Diego Freniche <diego@freniche.com>
2017-11-16 20:24:23 +0100

    First version

--- CrashLet/AppDelegate.swift
+++ CrashLet/AppDelegate.swift
@@ -12,16 +12,68 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet var view: NSView!
+    @IBOutlet weak var label: NSTextField!
+    @IBOutlet weak var menu: NSMenu!
+    
+    var end: Bool = false
+    
+    var timer: Timer?
+    var showing: Bool = true
+    var statusItem: NSStatusItem?
 
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        self.view.wantsLayer = true
+        self.view.layer?.cornerRadius = 20.0
+        self.label.layer?.cornerRadius = 20.0
+        self.view.layer?.backgroundColor = NSColor.clear.cgColor
+        self.window.backgroundColor = NSColor.clear
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        self.window.isOpaque = false
+        addIconToStatusBar()
+        
+        mainLoop()
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    func addIconToStatusBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.image = NSImage(named: NSImage.Name(rawValue: "skull-n-bones"))
+        statusItem?.menu = self.menu
     }
+    
+    @objc func mainLoop() {
+        if showing {
+        } else {
+        }
+        
+        self.showing = !self.showing
+        showWindow(self.window, fromAppDelegate: self)
 
-
+        let randomNumber = 10 + arc4random() % 60
+        print("Random \( randomNumber )")
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(randomNumber), target: self, selector: #selector(AppDelegate.mainLoop), userInfo: nil, repeats: false)
+    }
+    
+    func showWindow(_ window: NSWindow, fromAppDelegate: NSApplicationDelegate) {
+        window.makeKeyAndOrderFront(fromAppDelegate)
+        NSApp.activate(ignoringOtherApps: true)
+        NSAnimationContext.runAnimationGroup({ (context) -> Void in
+            context.duration = 1.5
+            window.animator().alphaValue = 1
+        }, completionHandler: { [weak self] () -> Void in
+            sleep(1)
+            self?.hideWindow(window, fromAppDelegate: fromAppDelegate)
+        })
+    }
+    
+    func hideWindow(_ window: NSWindow, fromAppDelegate: NSApplicationDelegate) {
+        NSAnimationContext.runAnimationGroup({ (context) -> Void in
+            context.duration = 1.5
+            window.animator().alphaValue = 0
+        }, completionHandler: { () -> Void in
+            window.orderOut(fromAppDelegate)
+        })
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,34 @@
+# CrashLet: for those who remember "let" mutable Arrays in Swift
+
+![](img/SourceKitCrash.png)
+
+If you've been using Swift from the very beginning, maybe you remember that dreaded message when the compiler crashed and no code completion / systax coloring worked at all. CrashLet brings you first-class crash Swift experiences, now system-wide.
+
+With CrashLet you can experience that loved message from time to while, while you read your mail, surf the Internet or listen to music in Itunes. It works even while you're developing with Android Studio, just to remind you about how good Swift is at crashing!
+
+![](img/github.png)
+_Crashlet while Githubbing_
+
+![](img/itunes.png)
+_Do you want to listen to music? Crash!_
+
+## Who's responsible for this
+
+As a researcher in recent, very recent Apple history, [Diego Freniche](http://www.github.com/dfreniche/cv) takes pride in giving you the most accurate Swift crashing emulation experience to date. 
+
+Other fine software projects hosted here are [Objective Doge](https://github.com/dfreniche/objective-doge), such extension of the Objective C language!, Wow!, Very clareness, much coding and Swift examples including [The Star Wars Operators](https://github.com/dfreniche/Playgrounds/blob/master/StarWarsOperators.playground/section-1.swift) 
+
+## Install
+
+- clone this repo
+- build in Xcode
+
+After all, this is a toy for developers, right?
+
+## Sharing
+
+Feel free to fork this repo, use the code in it or add new features through Pull Requests. Also, if you spot a typo in the documentation, or a bug in the app, please open an Issue. You don't even need to program to contribute to CrashLet, just create a video tutorial on how to use it, recommend ways of crash Swift more often, etc.
+
+This work is licensed under a MIT license.
+
+No `lldb` compilers where harmed during the writing of CrashLet.
